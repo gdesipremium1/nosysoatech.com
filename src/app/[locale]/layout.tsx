bc862e4ced4885c33e1ed921ff8580507de3bf4c
Désiré RABE <fetraniainadesirerabemanantsoa@gmail.com>
2026-09-16 16:11:23 +0200

    Configuration google analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Fraunces, IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import "../globals.css";
+
+const GA_MEASUREMENT_ID = "G-EQV1HQDVKE";
 
 const fraunces = Fraunces({
   variable: "--font-display",
@@ -64,6 +67,18 @@ export default async function LocaleLayout({
       className={`${fraunces.variable} ${plexSans.variable} ${plexMono.variable}`}
     >
       <body>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
     </html>
